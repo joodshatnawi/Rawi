c2cd46f87be195661e2293a688b4dac1915cf082
Jood mohammad <joodshatnawi85@gmail.com>
2026-08-22 17:15:59 +0300

    Create info_labels.py

---
+++ data/info_labels.py
@@ -0,0 +1,32 @@
+
+info_labels = {
+    "العربية": {
+        "built": "🏛️ تاريخ البناء",
+        "governorate": "📍 المحافظة",
+        "unesco": "⭐ اليونسكو",
+        "best_time": "⏰ أفضل وقت",
+        "visit_time": "🕒 مدة الزيارة",
+        "Yes": "نعم",
+        "No": "لا"
+    },
+
+    "English": {
+        "built": "🏛️ Built",
+        "governorate": "📍 Governorate",
+        "unesco": "⭐ UNESCO",
+        "best_time": "⏰ Best Time",
+        "visit_time": "🕒 Visit Time",
+        "Yes": "Yes",
+        "No": "No"
+    },
+
+    "Français": {
+        "built": "🏛️ Construction",
+        "governorate": "📍 Gouvernorat",
+        "unesco": "⭐ UNESCO",
+        "best_time": "⏰ Meilleure période",
+        "visit_time": "🕒 Durée de visite",
+        "Yes": "Oui",
+        "No": "Non"
+    }
+}
